6dde43aef051277a637166423915aaae76f7d494
AliceBan111 <alice.ban@gmail.com>
2026-03-05 00:46:40 +0100

    update with TFP

--- main.jl
+++ main.jl
@@ -1,6 +1,7 @@
 # Include utility functions
 include("src/ToolBox/load_var.jl")
 include("src/data/build_macro_data.jl")
+include("src/data/build_macro_data_with_TFP.jl")
 include("src/data/build_markup_data_method1.jl")
 include("src/data/build_markup_data_method2.jl")
 include("src/data/build_markup_data_method2_no_hp.jl")
@@ -13,24 +14,25 @@ using Dates, Plots
 cd(@__DIR__)
 
 # 1. Load macro data
-start_date_method1 = Date("1964-01-01")
-end_date_method1   = Date("2017-12-31")
+# start_date_method1 = Date("1964-01-01")
+# end_date_method1   = Date("2017-12-31")
 
-start_date_method2 = Date("1987-01-01")
-end_date_method2   = Date("2012-12-31")
+# start_date_method2 = Date("1987-01-01")
+# end_date_method2   = Date("2012-12-31")
 
 start_date_method2_latest = Date("1997-01-01")
 end_date_method2_latest   = Date("2023-12-31")
 
-macro_df_method1 = build_macro_data(start_date_method1, end_date_method1)
-macro_df_method2 = build_macro_data(start_date_method2, end_date_method2)
-macro_df_method2_no_hp = build_macro_data(start_date_method2, end_date_method2)
-macro_df_method2_latest = build_macro_data(start_date_method2_latest, end_date_method2_latest)
+# macro_df_method1 = build_macro_data(start_date_method1, end_date_method1)
+# macro_df_method2 = build_macro_data(start_date_method2, end_date_method2)
+# macro_df_method2_no_hp = build_macro_data(start_date_method2, end_date_method2)
+# macro_df_method2_latest = build_macro_data(start_date_method2_latest, end_date_method2_latest)
+macro_df_method2_latest = build_macro_data_with_TFP(start_date_method2_latest, end_date_method2_latest)
 
 # 2. Build markup data
-df_method1 = build_markup_method1(macro_df_method1)
-df_method2 = build_markup_method2(macro_df_method2)
-df_method2_no_hp = build_markup_method2_no_hp(macro_df_method2_no_hp)
+# df_method1 = build_markup_method1(macro_df_method1)
+# df_method2 = build_markup_method2(macro_df_method2)
+# df_method2_no_hp = build_markup_method2_no_hp(macro_df_method2_no_hp)
 df_method2_no_hp_latest = build_markup_method2_no_hp_latest(macro_df_method2_latest, "data/KLEMS_latest.xlsx")
 
 # 2.5 Plot VAR inputs
@@ -44,11 +46,11 @@ plot_VAR_inputs(df_method2_no_hp_latest; tag="method2_no_hp_latest", use_growth 
 plot_VAR_inputs(df_method2_no_hp_latest; tag="method2_no_hp_latest", use_growth  = false)
 
 # 3. Estimate VAR + Sign Restrictions
-res_method1 = estimate_VAR_SR(df_method1; method_name="method1", use_growth  = true)
-res_method1 = estimate_VAR_SR(df_method1; method_name="method1", use_growth  = false)
-res_method2 = estimate_VAR_SR(df_method2; method_name="method2", use_growth  = true)
-res_method2 = estimate_VAR_SR(df_method2; method_name="method2", use_growth  = false)
-res_method2_no_hp = estimate_VAR_SR(df_method2_no_hp; method_name="method2_no_hp", use_growth  = true)
-res_method2_no_hp = estimate_VAR_SR(df_method2_no_hp; method_name="method2_no_hp", use_growth  = false)
+# res_method1 = estimate_VAR_SR(df_method1; method_name="method1", use_growth  = true)
+# res_method1 = estimate_VAR_SR(df_method1; method_name="method1", use_growth  = false)
+# res_method2 = estimate_VAR_SR(df_method2; method_name="method2", use_growth  = true)
+# res_method2 = estimate_VAR_SR(df_method2; method_name="method2", use_growth  = false)
+# res_method2_no_hp = estimate_VAR_SR(df_method2_no_hp; method_name="method2_no_hp", use_growth  = true)
+# res_method2_no_hp = estimate_VAR_SR(df_method2_no_hp; method_name="method2_no_hp", use_growth  = false)
 res_method2_no_hp_latest = estimate_VAR_SR(df_method2_no_hp_latest; method_name="method2_no_hp_latest", use_growth  = true)
 res_method2_no_hp_latest = estimate_VAR_SR(df_method2_no_hp_latest; method_name="method2_no_hp_latest", use_growth  = false)
